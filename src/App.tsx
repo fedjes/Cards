f6a7b62451fd394a57d8fcbdaaac9c42a524928b
fedjes <salvados78@mail.ru>
2024-01-18 15:19:28 +0300

    feat: add component2

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,4 @@
-// import { Card } from '@/components/ui/Card/card'
-// import { RadioS } from '@/components/ui/radio/radio'
-
-import { Tabs, TabsComponent } from '@/components/ui/tabs'
-
 export function App() {
-  // const data = [
-  //   { id: 'r1', value: 'default' },
-  //   { id: 'r2', value: 'comfortable' },
-  //   { id: 'r3', value: 'compact' },
-  //   { id: 'r4', value: 'four' },
-  // ]
-  const testSwitcher = [
-    { disabled: false, title: 'testTitle', value: 'Switcher' },
-    { disabled: true, title: 'testTitle1', value: 'Switcher' },
-    { disabled: false, title: 'testTitle2', value: 'Switcher' },
-  ]
-
   return (
     <div
       style={{
@@ -25,22 +8,6 @@ export function App() {
         height: '100vh',
         justifyContent: 'center',
       }}
-    >
-      {/*<Card>*/}
-      {/*  <Inp />*/}
-      {/*  <Inp />*/}
-      {/*  <Btn />*/}
-      {/*</Card>*/}
-      {/*<RadioS data={data}></RadioS>*/}
-      <TabsComponent tabs={testSwitcher} />
-    </div>
+    ></div>
   )
 }
-
-// const Inp = () => {
-//   return <input style={{ width: '100%' }} />
-// }
-//
-// const Btn = () => {
-//   return <button>click me</button>
-// }
